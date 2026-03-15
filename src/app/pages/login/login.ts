d177cb65e946e5566e60d6188a6976ecd7512986
Shivansh Kuchchal <shivanshbox@gmail.com>
2026-03-15 22:28:14 +0530

    feat(ui): add login page ui

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,7 +1,22 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-login',
-  template: `<p>This is Login</p>`,
+  imports: [
+    RouterLink,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
+  templateUrl: './login.html',
+  styleUrl: './login.scss',
 })
-export class Login {}
+export class Login {
+  passwordVisible = false;
+}
