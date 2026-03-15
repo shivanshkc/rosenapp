9c694eb7d296622cf08b45d8d96af8f9313f1f03
Shivansh Kuchchal <shivanshbox@gmail.com>
2026-03-15 22:13:47 +0530

    feat: setup pages and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', loadComponent: () => import('./pages/login/login').then(m => m.Login) },
+  { path: 'register', loadComponent: () => import('./pages/register/register').then(m => m.Register) },
+  { path: 'home', loadComponent: () => import('./pages/home/home').then(m => m.Home) },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+];
---
+++ src/app/pages/login/login.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  template: `<p>This is Login</p>`,
+})
+export class Login {}
