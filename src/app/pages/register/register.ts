f3484a7cceaeb445c00da531521ae3b290a081cd
Shivansh Kuchchal <shivanshbox@gmail.com>
2026-03-15 22:31:54 +0530

    feat(ui): add register page ui

--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -1,7 +1,23 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-register',
-  template: `<p>This is Register</p>`,
+  imports: [
+    RouterLink,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
+  templateUrl: './register.html',
+  styleUrl: './register.scss',
 })
-export class Register {}
+export class Register {
+  passwordVisible = false;
+  repeatPasswordVisible = false;
+}
